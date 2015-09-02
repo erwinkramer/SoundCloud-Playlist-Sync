dda4ba394c99dbf329c9c654516a5e03983fa976
r3verse <r3verse@gmail.com>
2015-09-02 11:27:22 +0200

    test

--- src/Soundcloud Playlist Downloader/Soundcloud Playlist Downloader/Program.cs
+++ src/Soundcloud Playlist Downloader/Soundcloud Playlist Downloader/Program.cs
@@ -17,6 +17,7 @@ namespace Soundcloud_Playlist_Downloader
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //test
         }
     }
 }
