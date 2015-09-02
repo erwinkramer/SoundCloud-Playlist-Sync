601fbf335988950b9687352724299a6579b350b7
r3verse <r3verse@gmail.com>
2015-09-02 11:29:03 +0200

    Revert "test"
    This reverts commit dda4ba394c99dbf329c9c654516a5e03983fa976.

--- src/Soundcloud Playlist Downloader/Soundcloud Playlist Downloader/Program.cs
+++ src/Soundcloud Playlist Downloader/Soundcloud Playlist Downloader/Program.cs
@@ -17,7 +17,6 @@ namespace Soundcloud_Playlist_Downloader
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //test
         }
     }
 }
