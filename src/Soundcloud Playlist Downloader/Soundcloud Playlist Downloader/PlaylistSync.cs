65e06e07da684d29ffae9264df4c1e8612282c74
r3verse <r3verse@gmail.com>
2015-09-16 10:47:17 +0200

    Fix a bug where a comma in a filename would break comparison with existing file.

--- src/Soundcloud Playlist Downloader/Soundcloud Playlist Downloader/PlaylistSync.cs
+++ src/Soundcloud Playlist Downloader/Soundcloud Playlist Downloader/PlaylistSync.cs
@@ -778,7 +778,8 @@ namespace Soundcloud_Playlist_Downloader
         {           
             if(csv != null && csv.IndexOf(',') >= 0)
             {
-                return csv.Split(',')[position]; //position 0 is streampath, position 1 is local path
+                //only make 1 split, as a comma (,) can be found in a song name!
+                return csv.Split(new[] { ',' }, 2)[position]; //position 0 is streampath, position 1 is local path
             }
             else
             {
