ac3ccefcd5bbc0285878a9b6cd178d231318536d
r3verse <r3verse@gmail.com>
2015-11-14 18:41:59 +0100

    2.3 stable release

--- Soundcloud Playlist Downloader/Form1.Designer.cs
+++ Soundcloud Playlist Downloader/Form1.Designer.cs
@@ -397,7 +397,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "SoundCloud Playlist Sync 2.2 Stable";
+            this.Text = "SoundCloud Playlist Sync 2.3 Stable";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
