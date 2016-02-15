a15e6950a6f85ba969952546ea750e246d6cc3de
Tomas Gužauskas <tomiscout@gmail.com>
2016-02-15 15:37:31 +0200

    Fix bug when song names contain "." (period symbol) Then it leads to TagLib exception and stops sync

--- Soundcloud Playlist Downloader/PlaylistSync.cs
+++ Soundcloud Playlist Downloader/PlaylistSync.cs
@@ -474,8 +474,7 @@ namespace Soundcloud_Playlist_Downloader
                             request.Method = "HEAD";
                             using (WebResponse response = request.GetResponse())
                             {
-                                extension = Path.GetExtension(response.Headers["Content-Disposition"]
-                                    .Replace("attachment;filename=", "").Replace("\"", ""));
+                                extension = "." + response.Headers["x-amz-meta-file-type"];
                             }
                         }
                         catch (Exception e)
@@ -491,8 +490,7 @@ namespace Soundcloud_Playlist_Downloader
                             request.Method = "HEAD";
                             using (WebResponse response = request.GetResponse())
                             {
-                                extension = Path.GetExtension(response.Headers["Content-Disposition"]
-                                    .Replace("attachment;filename=", "").Replace("\"", ""));
+                                extension = "." + response.Headers["x-amz-meta-file-type"];
                             }
                         }
                         var allowedFormats = new List<string>();
