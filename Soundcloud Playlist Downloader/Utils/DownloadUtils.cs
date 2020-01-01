1bb45317b676fcd39e3eab6114036317899ccd26
Erwin Kramer <erwinkramer@hotmail.com>
2020-01-01 19:31:01 +0100

    fix spaces

--- Soundcloud Playlist Downloader/Utils/DownloadUtils.cs
+++ Soundcloud Playlist Downloader/Utils/DownloadUtils.cs
@@ -37,8 +37,8 @@ namespace Soundcloud_Playlist_Downloader.Utils
         public static HttpClient CreateHttpClientWithBrowserheaders()
         {
             var httpClientWithBrowserheaders = new HttpClient();
-            httpClientWithBrowserheaders.DefaultRequestHeaders.Add("Accept-Language", " en-US");
-            httpClientWithBrowserheaders.DefaultRequestHeaders.Add("Accept", " text/html, application/xhtml+xml, */*");
+            httpClientWithBrowserheaders.DefaultRequestHeaders.Add("Accept-Language", "en-US");
+            httpClientWithBrowserheaders.DefaultRequestHeaders.Add("Accept", "text/html, application/xhtml+xml, */*");
             httpClientWithBrowserheaders.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36");
             return httpClientWithBrowserheaders;
         }
