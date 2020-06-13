e8d69eb9a50aa57efcea8e9511bc9b656445b4f6
Erwin Kramer <erwinkramer@hotmail.com>
2020-06-13 18:46:41 +0200

    v up

--- Soundcloud Playlist Downloader/Properties/AssemblyInfo.cs
+++ Soundcloud Playlist Downloader/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("47.0.0.0")]
+[assembly: AssemblyVersion("48.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
 [assembly: NeutralResourcesLanguage("")]
