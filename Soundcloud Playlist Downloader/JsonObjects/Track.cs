6349be4110ac59bdc0b8810d17b0d0d0ff2bbed6
Erwin Kramer <erwinkramer@hotmail.com>
2020-06-13 14:30:13 +0200

    make some track properties nullable

--- Soundcloud Playlist Downloader/JsonObjects/Track.cs
+++ Soundcloud Playlist Downloader/JsonObjects/Track.cs
@@ -13,10 +13,10 @@ namespace Soundcloud_Playlist_Downloader.JsonObjects
         public DateTime ModifiedDateTimeUtc { get; set; }
         // song is considered HD when there is a download_url available
         public bool IsHD { get; set; }
-        public int playback_count { get; set; }
-        public int download_count { get; set; }
-        public int favoritings_count { get; set; }
-        public int comment_count { get; set; }
+        public int? playback_count { get; set; }
+        public int? download_count { get; set; }
+        public int? favoritings_count { get; set; }
+        public int? comment_count { get; set; }
         public bool IsDownloaded { get; set; } = false;
         public int IndexFromSoundcloud { get; set; }
 
